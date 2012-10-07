80981c8a197d75164460bdfa8004e448ab095007
Alexander Shabalin <shabalyn.a@gmail.com>
2012-10-07 23:37:13 +0400

    Sudden jump to nouveau but now with working brightness after sleep

--- etc/acpi/handler.sh
+++ etc/acpi/handler.sh
@@ -5,13 +5,10 @@ set $*
 case "$1" in
    button/power)
       logger "ACPI action: power button($2 $3 $4)"
-      systemctl hibernate
+      systemctl suspend
       ;;
    button/sleep)
       logger "ACPI action: sleep button($2 $3 $4)"
-      #Backlight control stops working. Yet! Starts working after
-      #I put it into sleep and then hibernation. WTF?
-      systemctl suspend
       ;;
    ac_adapter)
       logger "ACPI action: adapter($2 $3 $4)"
@@ -23,18 +20,18 @@ case "$1" in
       logger "ACPI action: lid($2 $3 $4)"
       ;;
    video/brightnessup)
-      path=/sys/class/backlight/acpi_video0
+      path=/sys/class/backlight/nv_backlight
       max=`cat $path/max_brightness`
       cur=`cat $path/brightness`
       if [[ $cur -lt $max ]]; then
-         echo $((cur + 1)) > $path/brightness
+         echo $((cur + 10)) > $path/brightness
       fi
       ;;
    video/brightnessdown)
-      path=/sys/class/backlight/acpi_video0
+      path=/sys/class/backlight/nv_backlight
       cur=`cat $path/brightness`
       if [[ $cur -gt 0 ]]; then
-         echo $((cur - 1)) > $path/brightness
+         echo $((cur - 10)) > $path/brightness
       fi
       ;;
    *)
